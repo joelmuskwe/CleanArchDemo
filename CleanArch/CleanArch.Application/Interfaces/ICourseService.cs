0c75b09995626df99e1eeaba6b2e67d8f4814891
Joel Muskwe <joel.muskwe@econet.co.zw>
2020-05-20 17:00:31 +0200

    Commit Service and Repository Interfaces,CourseViewModels

---
+++ CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using CleanArch.Application.ViewModels;
+
+namespace CleanArch.Application.Interfaces
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+}
---
+++ CleanArch/CleanArch.Application/ViewModels/CourseViewModel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using CleanArch.Domain.Models;
+
+namespace CleanArch.Application.ViewModels
+{
+    public class CourseViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
