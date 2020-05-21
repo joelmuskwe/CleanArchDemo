6a312e80b55b1e0bb8fe13d1bacc5e86977f2f2c
Joel Muskwe <joel.muskwe@econet.co.zw>
2020-05-21 09:38:54 +0200

    Command, CommandHandlers, Updated Course Repository, Cleaned up IoC Container

--- CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -18,5 +18,11 @@ namespace CleanArch.Infra.Data.Repository
         {
             return _context.Courses;
         }
+
+        public void Add(Course course)
+        {
+            _context.Courses.Add(course);
+            _context.SaveChanges();
+        }
     }
 }
--- CleanArch/CleanArch.Infra.IoC/DependencyContainter.cs
+++ CleanArch/CleanArch.Infra.IoC/DependencyContainter.cs
@@ -1,22 +1,34 @@
 using CleanArch.Application.Interfaces;
 using CleanArch.Application.Services;
+using CleanArch.Domain.CommandHandlers;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Bus;
+using CleanArch.Infra.Data.Context;
 using CleanArch.Infra.Data.Repository;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CleanArch.Infra.IoC
 {
     public static class DependencyContainer
     {
-        public static IServiceCollection RegisterServices(IServiceCollection services)
+        public static void RegisterServices(IServiceCollection services)
         {
+            // Domain InMemoryBus MediatR
+            services.AddScoped<IMediatorHandler, InMemoryBus>();
+            
+            // Domain Handlers
+            services.AddScoped<IRequestHandler<CreateCourseCommand, bool>, CourseCommandHandler>();
+            
             // Application Layer
             services.AddScoped<ICourseService, CourseService>();
             
             // Infra.Data Layer
             services.AddScoped<ICourseRepository, CourseRepository>();
-            
-            return services;
+
+            services.AddScoped<UniversityDbContext>();
         }
     }
 }
